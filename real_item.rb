37844e7542333dd6da4ed38b5388ab842a881b1b
Sasha Matviy <omatviy@gmail.com>
2022-05-05 22:20:14 +0300

    fixing code

--- real_item.rb
+++ real_item.rb
@@ -1,3 +1,21 @@
-class RealItem
-    
-end
+class RealItem < Item
+    attr_accessor :eight
+  
+    def initialize(options = {})
+      @weight = options[:weight] if options[:weight]
+      super
+    end
+  
+    def info
+      if block_given?
+        puts "RealItem info start:"
+        yield @weight, "item weight:"
+        super
+        puts "RealItemItem info finish"
+      else
+        puts "Nothing to show"
+      end
+    end
+  
+  end
+  

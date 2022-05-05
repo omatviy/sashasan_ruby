6e0afa5e023e2c7cb65271c3e0ed1de24b6a104a
Sasha Matviy <omatviy@gmail.com>
2022-05-05 18:49:12 +0300

    adding code to  class Cart

--- cart.rb
+++ cart.rb
@@ -1,19 +1,22 @@
-class Cart 
-    attr_reader :items
+class Cart
+  attr_reader :items
 
-    def initialize
+  def initialize
+    @items = []
+  end
 
+  def add_item(item)
+    @items << item
+  end
+
+  def remove_item
+    @items.pop
+  end
+
+  def validate
+    @items.each do |i|
+        puts "item has no price" if i.price.nil?
     end
-
-    def add_item
-
-    end
-
-    def remove_item
-
-    end
-
-    def validate
-        
-    end
+  end
+  
 end

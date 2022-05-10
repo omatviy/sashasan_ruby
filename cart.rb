037507dbde5a17dbe7cf32be4b699117aec18dff
Sasha Matviy <omatviy@gmail.com>
2022-05-10 14:31:51 +0300

    Comment lesson 23

--- cart.rb
+++ cart.rb
@@ -2,7 +2,8 @@ require_relative "item_container.rb"
 
 class Cart
   attr_reader :items
-  include ItemContainer::Manager
+  # include ItemContainer::Manager
+  include ItemContainer
 
   def initialize
     puts "Cart init"
--- item_container.rb
+++ item_container.rb
@@ -1,27 +1,29 @@
 module ItemContainer
-  module Manager
-    def add_item(item)
-      @items << item
-    end
+  #  module Manager
+  def add_item(item)
+    @items << item
+  end
 
-    def remove_item
-      @items.pop
-    end
+  def remove_item
+    @items.pop
+  end
 
-    def validate
-      @items.each do |i|
-        puts "item has no price" if i.price.nil?
-      end
-    end
-
-    def delete_invalid_items
-      @items.delete_if { |i| i.price.nil? }
+  def validate
+    @items.each do |i|
+      puts "item has no price" if i.price.nil?
     end
   end
 
-  module Info
-    def count_valid_items
-      @items.count { |item| item.price }
-    end
+  def delete_invalid_items
+    @items.delete_if { |i| i.price.nil? }
   end
+
+  # end
+
+  #  module Info
+  def count_valid_items
+    @items.count { |item| item.price }
+  end
+
+  #  end
 end
